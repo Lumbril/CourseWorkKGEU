27a4ccdc5f6ebf05a90860573c0427fd684329dc
Lumbril <32036622+Lumbril@users.noreply.github.com>
2021-03-30 21:03:55 +0300

    refact main.cpp

--- main.cpp
+++ main.cpp
@@ -1,111 +1,97 @@
 #include <iostream>
 using namespace std;
 
-const char LEFT_BRACKET = '(';
-const char RIGHT_BRACKET = ')';
-const char OP_PLUS = '+';
-const char OP_MINUS = '-';
-const char OP_MULT = '*';
-const char OP_DIV = '/';
-const char NUMBER = 'N';
-const char END_STR = 'E';
+// список лексем
+enum LexemeType{
+    LEFT_BRACKET, RIGHT_BRACKET,
+    OP_PLUS, OP_MINUS, OP_MULT, OP_DIV,
+    NUMBER,
+    END_STR
+};
 
+// структура для хранения одной лексемы
+// LexemeType - тип лексемы
+// val - ее значение
 struct Lexeme {
-    char type;
+    LexemeType type;
     string val;
 };
 
-Lexeme lexems[100000];
-int posLexems = 0;
+// массив лексем
+Lexeme lexemes[100000];
+int posLexemes = 0;
+int szL;
 
-Lexeme lexemBuffer[100000];
-int posLB = 0;
-int szLB;
-
-void initLB() {
-    szLB = posLexems;
-
-    for (int i = 0; i < posLexems; i++) {
-        lexemBuffer[i] = lexems[i];
-    }
+// функция для получения следующей лексемы
+Lexeme nextLexeme() {
+    return lexemes[posLexemes++];
 }
 
-Lexeme nextLB() {
-    return lexemBuffer[posLB++];
+// метод для возрата индекса на одну позицию назад
+void backPosLex() {
+    posLexemes--;
 }
 
-void backLB() {
-    posLB--;
+// функция для получения текущего индекса
+int getPosLex() {
+    return posLexemes;
 }
 
-int getPosLB() {
-    return posLB;
+// метод для инициализации массива лексем
+void lexemeAnalyze(string expText);
+
+// функция для оптимизации выражений с отриц. числами.
+// пример, -3 преобразует в (0 - 3)
+string optimize(string s);
+
+// основные функции для анализа мат. выражения
+int expr();
+int plusminus();
+int multdiv();
+int factor();
+
+//функция для замены x, y на их значения и вычисление результата
+int form(string expression, int x, int y);
+
+int main() {
+    string expression; // строка для хранения мат. выражения
+
+    cout << "Expression: ";
+    getline(cin, expression);
+
+    int x, y;
+
+    cout << "x: ";
+    cin >> x;
+    cout << "y: ";
+    cin >> y;
+
+    int ans = form(expression, x, y); // вычисление результата
+
+    cout << "Answer: " << ans;
+
+    return 0;
 }
 
-void lexexAnalyze(string expText) {
-    int pos = 0;
-
-    while (pos < expText.size()) {
-        char ch = expText[pos];
-
-        switch (ch) {
-            case LEFT_BRACKET:
-                lexems[posLexems++] = {LEFT_BRACKET, "("};
-                pos++;
-
-                break;
-            case RIGHT_BRACKET:
-                lexems[posLexems++] = {RIGHT_BRACKET, ")"};
-                pos++;
-
-                break;
-            case OP_PLUS:
-                lexems[posLexems++] = {OP_PLUS, "+"};
-                pos++;
-
-                break;
-            case OP_MINUS:
-                lexems[posLexems++] = {OP_MINUS, "-"};
-                pos++;
-
-                break;
-            case OP_MULT:
-                lexems[posLexems++] = {OP_MULT, "*"};
-                pos++;
-
-                break;
-            case OP_DIV:
-                lexems[posLexems++] = {OP_DIV, "/"};
-                pos++;
-
-                break;
-            default:
-                if ('0' <= ch && ch <= '9') {
-                    string buf = "";
-
-                    do {
-                        buf += ch;
-                        pos++;
-
-                        if (pos >= expText.size()) {
-                            break;
-                        }
-
-                        ch = expText[pos];
-                    } while ('0' <= ch && ch <= '9');
-
-                    lexems[posLexems++] = {NUMBER, buf};
-                } else {
-                    if (ch != ' ') {
-                        throw ("Invalid character" + ch);
-                    }
-
-                    pos++;
-                }
-        }
+int form(string expression, int x, int y) {
+    // замена переменных на их значения
+    while (expression.find('x') != string::npos) {
+        expression.replace(expression.find('x'), 1, to_string(x));
     }
 
-    lexems[posLexems++] = {END_STR, "END"};
+    while (expression.find('y') != string::npos) {
+        expression.replace(expression.find('y'), 1, to_string(y));
+    }
+
+    // оптимизация отриц. чисел
+    expression = optimize(expression);
+
+    // инициализация массива лексем
+    lexemeAnalyze(expression);
+
+    int ans = expr();
+
+    return ans;
 }
 
 string optimize(string s) {
@@ -140,18 +126,80 @@ string optimize(string s) {
     return s;
 }
 
-int expr();
-int plusminus();
-int multdiv();
-int factor();
+void lexemeAnalyze(string expText) {
+    int pos = 0;
+
+    while (pos < expText.size()) {
+        char ch = expText[pos];
+
+        switch (ch) {
+            case '(':
+                lexemes[posLexemes++] = {LEFT_BRACKET, "("};
+                pos++;
+
+                break;
+            case ')':
+                lexemes[posLexemes++] = {RIGHT_BRACKET, ")"};
+                pos++;
+
+                break;
+            case '+':
+                lexemes[posLexemes++] = {OP_PLUS, "+"};
+                pos++;
+
+                break;
+            case '-':
+                lexemes[posLexemes++] = {OP_MINUS, "-"};
+                pos++;
+
+                break;
+            case '*':
+                lexemes[posLexemes++] = {OP_MULT, "*"};
+                pos++;
+
+                break;
+            case '/':
+                lexemes[posLexemes++] = {OP_DIV, "/"};
+                pos++;
+
+                break;
+            default:
+                if ('0' <= ch && ch <= '9') {
+                    string buf = "";
+
+                    do {
+                        buf += ch;
+                        pos++;
+
+                        if (pos >= expText.size()) {
+                            break;
+                        }
+
+                        ch = expText[pos];
+                    } while ('0' <= ch && ch <= '9');
+
+                    lexemes[posLexemes++] = {NUMBER, buf};
+                } else {
+                    if (ch != ' ') {
+                        throw ("Invalid character" + ch);
+                    }
+
+                    pos++;
+                }
+        }
+    }
+
+    lexemes[posLexemes] = {END_STR, "END"};
+    posLexemes = 0;
+}
 
 int expr() {
-    Lexeme lexeme = nextLB();
+    Lexeme lexeme = nextLexeme();
 
     if (lexeme.type == END_STR) {
         return 0;
     } else {
-        backLB();
+        backPosLex();
 
         return plusminus();
     }
@@ -161,7 +209,7 @@ int plusminus() {
     int val = multdiv();
 
     while (1) {
-        Lexeme lexeme = nextLB();
+        Lexeme lexeme = nextLexeme();
 
         switch (lexeme.type) {
             case OP_PLUS:
@@ -173,7 +221,7 @@ int plusminus() {
 
                 break;
             default:
-                backLB();
+                backPosLex();
 
                 return val;
         }
@@ -184,7 +232,7 @@ int multdiv() {
     int val = factor();
 
     while (1) {
-        Lexeme lexeme = nextLB();
+        Lexeme lexeme = nextLexeme();
 
         switch (lexeme.type) {
             case OP_MULT:
@@ -196,7 +244,7 @@ int multdiv() {
 
                 break;
             default:
-                backLB();
+                backPosLex();
 
                 return val;
         }
@@ -204,7 +252,7 @@ int multdiv() {
 }
 
 int factor() {
-    Lexeme lexeme = nextLB();
+    Lexeme lexeme = nextLexeme();
     int val;
 
     switch (lexeme.type) {
@@ -212,7 +260,7 @@ int factor() {
             return atoi(lexeme.val.c_str());
         case LEFT_BRACKET:
             val = expr();
-            lexeme = nextLB();
+            lexeme = nextLexeme();
 
             if (lexeme.type != RIGHT_BRACKET) {
                 throw ("Forgot bracket");
@@ -222,43 +270,4 @@ int factor() {
         default:
             throw ("Incorrect entry");
     }
-}
-
-int Form(string expression, int x, int y) {
-    while (expression.find('x') != string :: npos) {
-        expression.replace(expression.find('x'), 1, to_string(x));
-    }
-
-    while (expression.find('y') != string :: npos) {
-        expression.replace(expression.find('y'), 1, to_string(y));
-    }
-
-    expression = optimize(expression);
-
-    lexexAnalyze(expression);
-
-    initLB();
-    int ans = expr();
-
-    return ans;
-}
-
-int main() {
-    string expression;
-
-    cout << "Expression: ";
-    getline(cin, expression); // ������ �������������� ���������
-
-    int x, y;
-
-    cout << "x: ";
-    cin >> x;
-    cout << "y: ";
-    cin >> y;
-
-    int ans = Form(expression, x, y);
-
-    cout << "Answer: " << ans;
-
-    return 0;
 }
